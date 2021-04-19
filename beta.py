d5b3e5ca1d2d6c52ba4770dbfc5ed718a02d268f
M Vetrivel <vetrisurabi@gmail.com>
2021-04-19 15:08:13 +0530

    PS

--- beta.py
+++ beta.py
@@ -1,17 +1,56 @@
 import pandas as pd 
 import os
 
-df=pd.read_csv(r'{0}/all_matches.csv'.format(os.path.dirname(os.path.abspath(__file__))),low_memory=False)
-beta_df=pd.DataFrame()
-run=pd.DataFrame()
-new=pd.DataFrame()
-for id in range(10):
-    new=df.loc[df[['match_id']][id]]
+
+def function(matchid,innings):
+    
+    df = pd.read_csv(r'{0}/all_matches.csv'.format(os.path.dirname(os.path.abspath(__file__))),low_memory=False)
+    
+
+    new = df.loc[df['match_id']==matchid]
     new = new.loc[new['ball']<=5.6]
-    run=new.groupby(['innings'])[['runs_off_bat','extras','wides','noballs','byes','legbyes']].sum()
-    run['totalscore']=run['runs_off_bat']+run['wides']+run['noballs']+run['byes']+run['legbyes']
-    if id==0:
-        beta_df=run
-    else:
-        beta_df=pd.concat([beta_df,run])
-print(beta_df)
+
+    run = new.groupby(['innings'])[['runs_off_bat','extras','wides','noballs','byes','legbyes']].sum()
+
+    run['totalscore'] = run['runs_off_bat']+run['wides']+run['noballs']+run['byes']+run['legbyes']
+
+
+    run['innings'] = [1,2]
+
+    run['wickets'] = new.groupby(['innings'])[['player_dismissed']].count()
+
+
+    return(run.loc[run['innings']==innings])
+
+
+id = 335982
+
+a = function(id,1)
+
+innings,score,wickets = [i for i in a['innings']],[i for i in a['totalscore']],[i for i in a['wickets']]
+
+print(innings,score,wickets)
+
+
+
+
+
+#run = pd.DataFrame()
+#new = pd.DataFrame()
+
+
+
+
+
+
+
+# for id in range(10):
+#     new=df.loc[df[['match_id']]]]
+#     new = new.loc[new['ball']<=5.6]
+#     run=new.groupby(['innings'])[['runs_off_bat','extras','wides','noballs','byes','legbyes']].sum()
+#     run['totalscore']=run['runs_off_bat']+run['wides']+run['noballs']+run['byes']+run['legbyes']
+#     if id==0:
+#         beta_df=run
+#     else:
+#         beta_df=pd.concat([beta_df,run])
+# print(beta_df)
