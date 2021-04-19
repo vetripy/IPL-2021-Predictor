b54eaeb57e24488e809cd8b61fd5679c8216536d
M Vetrivel <vetrisurabi@gmail.com>
2021-04-19 13:22:44 +0530

    Chumma

--- dataframe.py
+++ dataframe.py
@@ -4,6 +4,7 @@ import os
 df=pd.read_csv(r'{0}/all_matches.csv'.format(os.path.dirname(os.path.abspath(__file__))),low_memory=False)
 
 new=df.loc[df['match_id']==335982]
+new = new.loc[new['ball']<=5.6]
 run=new.groupby(['innings'])[['runs_off_bat','extras','wides','noballs','byes','legbyes']].sum()
 run['totalscore']=run['runs_off_bat']+run['wides']+run['noballs']+run['byes']+run['legbyes']
 
