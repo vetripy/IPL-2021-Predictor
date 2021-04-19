0d71b8987277600b2963535ed3b496783149334f
M Vetrivel <vetrisurabi@gmail.com>
2021-04-19 14:42:16 +0530

    Hmm

--- dataframe.py
+++ dataframe.py
@@ -1,13 +1,18 @@
 import pandas as pd 
 import os
 
+
 df=pd.read_csv(r'{0}/all_matches.csv'.format(os.path.dirname(os.path.abspath(__file__))),low_memory=False)
 
 new=df.loc[df['match_id']==335982]
 new = new.loc[new['ball']<=5.6]
+
 run=new.groupby(['innings'])[['runs_off_bat','extras','wides','noballs','byes','legbyes']].sum()
+
 run['totalscore']=run['runs_off_bat']+run['wides']+run['noballs']+run['byes']+run['legbyes']
 
+run['innings']=[1,2]
 
 run['wickets']=new.groupby(['innings'])[['player_dismissed']].count()
-print(run)
+
+print(run[['innings','totalscore','wickets']].to_string(index=False))
