ba70757a4a23d75bf223efbab3d1e5f6a0ee7ff4
P S RAGHUL <raghulps023@gmail.com>
2021-04-19 13:19:57 +0530

    success

--- dataframe.py
+++ dataframe.py
@@ -7,18 +7,6 @@ new=df.loc[df['match_id']==335982]
 run=new.groupby(['innings'])[['runs_off_bat','extras','wides','noballs','byes','legbyes']].sum()
 run['totalscore']=run['runs_off_bat']+run['wides']+run['noballs']+run['byes']+run['legbyes']
 
-first_innings_wickets = 0
-second_innings_wickets = 0
 
-for i in new['player_dismissed']:
-    if type(i)==str:
-        a = new[new['player_dismissed']==i]['innings']
-        for i in a:
-            if i == 1:
-                first_innings_wickets+=1
-            if i == 2:
-                second_innings_wickets+=1
-
-print(first_innings_wickets,second_innings_wickets)
 run['wickets']=new.groupby(['innings'])[['player_dismissed']].count()
 print(run)
