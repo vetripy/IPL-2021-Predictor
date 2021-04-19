8338a19be20b492d14c9139dea4bc89ab9ba3292
P S RAGHUL <raghulps023@gmail.com>
2021-04-19 15:09:58 +0530

    ass

--- beta.py
+++ beta.py
@@ -5,12 +5,12 @@ df=pd.read_csv(r'{0}/all_matches.csv'.format(os.path.dirname(os.path.abspath(__f
 beta_df=pd.DataFrame()
 run=pd.DataFrame()
 new=pd.DataFrame()
-for id in range(10):
-    new=df.loc[df[['match_id']][id]]
+for id in df[['match_id']]:
+    new=df.loc[df['match_id']==id]
     new = new.loc[new['ball']<=5.6]
     run=new.groupby(['innings'])[['runs_off_bat','extras','wides','noballs','byes','legbyes']].sum()
     run['totalscore']=run['runs_off_bat']+run['wides']+run['noballs']+run['byes']+run['legbyes']
-    if id==0:
+    if id==335982:
         beta_df=run
     else:
         beta_df=pd.concat([beta_df,run])
