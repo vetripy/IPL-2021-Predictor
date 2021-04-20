0df9fb1035369098ea304a16724d798ea418a18c
P S RAGHUL <raghulps023@gmail.com>
2021-04-20 15:28:35 +0530

    done

--- dataframe.py
+++ dataframe.py
@@ -10,9 +10,9 @@ df = pd.read_csv(r'{0}/csv/all_matches.csv'.format(os.path.dirname(os.path.abspa
 def function(matchid,innings):
 
     new = df.loc[df['match_id']==i]
-
-    total_run = new
-
+    total_run=pd.DataFrame()
+    total_run=total_run.append(new)
+    
     new = new.loc[new['ball']<=5.6]
 
     new.drop(new[new['innings']>2].index,inplace=True)
@@ -26,17 +26,18 @@ def function(matchid,innings):
     else:
 
         run = new.groupby(['innings'])[['runs_off_bat','extras','wides','noballs','byes','legbyes']].sum()
+        
         total_run = total_run.groupby(['innings'])[['runs_off_bat','extras','wides','noballs','byes','legbyes']].sum()
 
-        total_run = total_run['runs_off_bat']+total_run['wides']+total_run['noballs']+total_run['byes']+total_run['legbyes']
-
+        run['target_score'] = total_run['runs_off_bat']+total_run['wides']+total_run['noballs']+total_run['byes']+total_run['legbyes']
+        
         run['totalscore'] = run['runs_off_bat']+run['wides']+run['noballs']+run['byes']+run['legbyes']
 
         balls = new.groupby(['runs_off_bat','innings']).size().reset_index(name='counts')
 
         run['match_id'] = matchid
         
-        run['target_score'] = total_run
+        
         
         #DOT-BALL COUNT
         if innings==1:
@@ -69,10 +70,9 @@ def function(matchid,innings):
         
         run['wickets'] = new.groupby(['innings'])[['player_dismissed']].count()
         
-
-        return(run[['match_id','innings','target_score','totalscore','wickets','dot_balls','boundary']].loc[run['innings']==innings])
         
-
+        return(run[['match_id','innings','totalscore','target_score','wickets','dot_balls','boundary']].loc[run['innings']==innings])
+        
 ids = [i for i in df['match_id'].unique()]
 
 
@@ -81,7 +81,7 @@ second_innings=pd.DataFrame()
 
 
 
-for i in ids[:1]:
+for i in ids[:3]:
     
     first = function(i,1)
     second = function(i,2)
@@ -89,22 +89,14 @@ for i in ids[:1]:
     if first.empty != True and second.empty != True:
 
         first_innings = first_innings.append(function(i,1))
-        
-        target_score = first_innings['target_score']
-        first_innings.drop('target_score',axis='columns',inplace=True)
-        
         second_innings = second_innings.append(function(i,2))
-        second_innings.drop('target_score',axis='columns',inplace=True)
         
-        second_innings.index = first_innings.index
-        second_innings = second_innings.assign(target_score)
         
-        #second_innings = second_innings.append(target)
+        
         
         
 
 
-print(second_innings)
 # first_innings = first_innings.reset_index(drop=True)
 # second_innings = second_innings.reset_index(drop=True)
 
