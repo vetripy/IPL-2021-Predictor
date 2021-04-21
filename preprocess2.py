e01b0d5a9768e6b12726bfa5a97d7de00ff93451
P S RAGHUL <raghulps023@gmail.com>
2021-04-21 17:33:36 +0530

    done

--- preprocess2.py
+++ preprocess2.py
@@ -28,26 +28,49 @@ def function(matchid):
         batsman['score'] = batsman['runs_off_bat']+batsman['extras']
         batsman = batsman.drop(columns=['runs_off_bat','extras','wides','noballs','byes','legbyes'])
 
+        balls = new.groupby(['runs_off_bat','innings','bowler']).size().reset_index(name='counts')
+        balls=balls[(balls.runs_off_bat==0)]
+        bowler = new.groupby(['venue', 'innings', 'batting_team','bowler'])[['player_dismissed','wides','noballs']].count()
+        bowler['dot_balls'] = balls['counts'].values
+        bowler['extra']=bowler['wides']+bowler['noballs']
+        bowler = bowler.drop(columns=['wides','noballs'])
+        
+        return(batsman,bowler)
 
-        return(batsman)
+
+def bowling_stats():
+    bowler_stats=pd.DataFrame()
+    bowler_stats = bowler_stats.append(df)
+   
+    bowler_stats=bowler_stats.groupby(['match_id','bowler','batting_team'],as_index=False)[['player_dismissed']].count()
+    bowler_stats=bowler_stats.groupby(['bowler','batting_team'],as_index=False)[['player_dismissed']].mean()
+    economy=df.groupby(['match_id','bowler','batting_team'],as_index=False)[['runs_off_bat','extras']].sum()
+    economy['economy']=(economy['runs_off_bat']+economy['extras']).div(4)
+    economy = economy.drop(columns=['runs_off_bat','extras'])
+    economy=economy.groupby(['bowler','batting_team'],as_index=False)[['economy']].mean()
+    bowler_stats['economy']=economy['economy'].values
+    bowler_stats['player_dismissed']=bowler_stats['player_dismissed'].astype(int)
+    return bowler_stats
+
 
 ids = [i for i in df['match_id'].unique()]
 
+bowler_stats=pd.DataFrame()
+bat_data = pd.DataFrame()
+ball_data = pd.DataFrame()
+bowler_stats=bowling_stats()
 
-match_data = pd.DataFrame()
 
-
-print(ids[-1])
 
 for i in ids[-2:]:
     
-    data = function(i)
-
+    databat,databall = function(i)
     
-    if data.empty != True:
+    
+    #if databat.empty != True and databall != True:
 
-        match_data = match_data.append(function(i))
-        
+    bat_data = bat_data.append(databat)
+    ball_data = ball_data.append(databall)
         
 
-print(match_data)
+print(bowler_stats)
